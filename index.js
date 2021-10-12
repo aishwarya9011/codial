af5c8ec1cb9ff3cc045b5491c8263df6e062b666
Aishwarya <ameshram2002@gmail.com>
2021-10-12 12:52:06 +0530

    setup static files

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts);
+app.use(express.static('./assets'));
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
 
 app.use('/',require('./routes'));
-
+app.set('view engine','ejs');
+app.set('views','./views');
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running server:${err}`);
